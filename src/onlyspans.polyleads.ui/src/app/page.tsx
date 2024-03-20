d4d6909b985d6f3da6162d272ee248e62e8cf2b3
milkyLake <ajnaliev07@gmail.com>
2024-03-20 13:57:22 +0300

    added prettier

--- src/onlyspans.polyleads.ui/src/app/page.tsx
+++ src/onlyspans.polyleads.ui/src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>ASd</h1>
-      <Button variant={"outline"}>Click me</Button>
+      <Button variant={'outline'}>Click me</Button>
     </main>
   );
 }
