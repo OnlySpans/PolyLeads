03da0e6ad5c2901e0f5ec2b5eff43e76c62aa7d5
Каралюс Никита <nkaralius@data-platform.ru>
2024-03-23 21:24:20 +0300

    Added projects to aspire

--- src/OnlySpans.Aspire.AppHost/Program.cs
+++ src/OnlySpans.Aspire.AppHost/Program.cs
@@ -1,4 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.Build()
+var api = builder
+   .AddProject<Projects.OnlySpans_PolyLeads_Api>("api");
+
+builder
+   .AddProject<Projects.OnlySpans_PolyLeads_ReverseProxy>("reverse-proxy")
+   .WithReference(api);
+
+builder
+   .Build()
    .Run();
