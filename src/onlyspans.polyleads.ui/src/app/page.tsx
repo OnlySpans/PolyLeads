8dd7db534405986d7748de06a70c5226c89ab986
milkyLake <ajnaliev07@gmail.com>
2024-03-23 11:06:35 +0300

    prawkii

--- src/onlyspans.polyleads.ui/src/app/page.tsx
+++ src/onlyspans.polyleads.ui/src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button';
 
-export default function Home() {
+const Home = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>ASd</h1>
       <Button variant={'outline'}>Click me</Button>
     </main>
   );
-}
+};
+
+export default Home;
