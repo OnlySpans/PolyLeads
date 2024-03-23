433131002987c191ff3fec028bc9a100237b3dd2
Каралюс Никита <nkaralius@data-platform.ru>
2024-03-23 19:58:59 +0300

    Added SearchablePdfRecognition

--- OnlySpans.PolyLeads.Api/Abstractions/IDocumentRecognition.cs
+++ OnlySpans.PolyLeads.Api/Abstractions/IDocumentRecognition.cs
@@ -1,8 +1,10 @@
+using OnlySpans.PolyLeads.Api.Data.Records.Recognition;
+
 namespace OnlySpans.PolyLeads.Api.Abstractions;
 
 public interface IDocumentRecognition
 {
-    Task RecognizeAsync(
+    Task<RecognitionResult> RecognizeAsync(
         Stream document,
         CancellationToken cancellationToken = new());
 }
---
+++ OnlySpans.PolyLeads.Api/Services/SearchablePdfRecognition.cs
@@ -0,0 +1,25 @@
+using OnlySpans.PolyLeads.Api.Abstractions;
+using OnlySpans.PolyLeads.Api.Data.Records.Recognition;
+using UglyToad.PdfPig;
+
+namespace OnlySpans.PolyLeads.Api.Services;
+
+public sealed class SearchablePdfRecognition :
+    IDocumentRecognition
+{
+    public Task<RecognitionResult> RecognizeAsync(
+        Stream document,
+        CancellationToken cancellationToken = new())
+    {
+        using var pdfDocument = PdfDocument.Open(document);
+
+        var pages = pdfDocument
+           .GetPages()
+           .Select(x => new RecognitionPage(x.Number, x.Text))
+           .ToList();
+
+        var recognitionResult = new RecognitionResult(pages);
+
+        return Task.FromResult(recognitionResult);
+    }
+}
