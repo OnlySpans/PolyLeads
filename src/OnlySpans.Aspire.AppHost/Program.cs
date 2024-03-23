7e4f7f24b90396f05622f457dce9b55e5c5a0079
Каралюс Никита <nkaralius@data-platform.ru>
2024-03-23 22:24:35 +0300

    Added postgres

--- src/OnlySpans.Aspire.AppHost/Program.cs
+++ src/OnlySpans.Aspire.AppHost/Program.cs
@@ -1,12 +1,17 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder
+    .AddPostgres("postgres", password: "postgres")
+    .WithVolumeMount("onlyspans-pd-data", "/var/lib/postgresql/data");
+
 var api = builder
-   .AddProject<Projects.OnlySpans_PolyLeads_Api>("api");
+    .AddProject<Projects.OnlySpans_PolyLeads_Api>("api")
+    .WithReference(postgres);
 
 builder
-   .AddProject<Projects.OnlySpans_PolyLeads_ReverseProxy>("reverse-proxy")
-   .WithReference(api);
+    .AddProject<Projects.OnlySpans_PolyLeads_ReverseProxy>("reverse-proxy")
+    .WithReference(api);
 
 builder
-   .Build()
-   .Run();
+    .Build()
+    .Run();
