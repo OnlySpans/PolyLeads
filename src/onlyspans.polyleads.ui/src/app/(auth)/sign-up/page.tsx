cfd2513940f339219cda3c96b77915e250439cff
milkyLake <ajnaliev07@gmail.com>
2024-04-08 16:56:39 +0300

    routing refactored

--- src/onlyspans.polyleads.ui/src/app/(auth)/sign-up/page.tsx
+++ src/onlyspans.polyleads.ui/src/app/(auth)/sign-up/page.tsx
@@ -1,39 +1,39 @@
-import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
-import SignInForm from '@/components/auth/sign-in/SignInForm';
+import Link from 'next/link';
 import { ThemeSwitchButton } from '@/components/ui/ThemeSwitchButton';
 import AuthBackgroundImage from '@/components/auth/AuthBackgroundImage';
+import SignUpForm from '@/components/auth/sign-up/SignUpForm';
 
-const AuthenticationPage = () => {
+const SignUpPage = () => {
   return (
     <div className='bg-fixed container relative hidden h-screen flex-col items-center justify-center min-[300px]:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
       <Link
-        href={'/auth/sign-up'}
+        href={'/sign-in'}
         className={cn(
           buttonVariants({ variant: 'ghost' }),
           'absolute right-4 top-4 md:right-8 md:top-8',
         )}
       >
-        Регистрация
+        Войти
       </Link>
       <div className={'absolute right-4 bottom-4 md:right-6 md:bottom-6'}>
         <ThemeSwitchButton />
       </div>
       <AuthBackgroundImage />
+
       <div className='lg:p-8'>
         <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
           <div className='flex flex-col space-y-2 text-center'>
-            <h1 className='text-2xl font-semibold tracking-tight'>Вход</h1>
-            <p className='text-sm text-muted-foreground'>
-              Используйте почту или имя пользователя
-            </p>
+            <h1 className='text-2xl font-semibold tracking-tight'>
+              Регистрация
+            </h1>
           </div>
-          <SignInForm />
+          <SignUpForm />
         </div>
       </div>
     </div>
   );
 };
 
-export default AuthenticationPage;
+export default SignUpPage;
--- src/onlyspans.polyleads.ui/src/app/page.tsx
+++ src/onlyspans.polyleads.ui/src/app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <Link
-        href={'/auth/sign-in'}
+        href={'/sign-in'}
         className={cn(buttonVariants({ variant: 'default' }))}
       >
         Войти
