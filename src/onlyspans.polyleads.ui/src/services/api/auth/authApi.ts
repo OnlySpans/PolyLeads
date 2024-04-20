2a0a0799ee8caf9e460f555afdaa24f8329aa6f5
Каралюс Никита <nkaralius@data-platform.ru>
2024-04-20 17:20:48 +0300

    Added signup

--- src/onlyspans.polyleads.ui/src/services/api/auth/authApi.ts
+++ src/onlyspans.polyleads.ui/src/services/api/auth/authApi.ts
@@ -1,20 +1,30 @@
 import ApiClientBase from '@/services/api/api.base';
 import Endpoints from '@/services/api/endpoints';
+import { ISignInPayload } from '@/data/abstractions/ISignInPayload';
+import { ISignUpPayload } from '@/data/abstractions/ISignUpPayload';
 
 export interface IAuthApi {
-  singin(username: string, password: string): Promise<boolean>;
+  signin(payload: ISignInPayload): Promise<boolean>;
+  signup(payload: ISignUpPayload): Promise<boolean>;
 }
 
 export class AuthApi
   extends ApiClientBase
   implements IAuthApi {
-  public readonly singin = async (
-    username: string,
-    password: string
+
+  public readonly signin = async (
+    { password, username }: ISignInPayload
   ): Promise<boolean> => {
     const url = Endpoints.Auth.signin();
     const response = await this.asyncRunner(
       () => this.api.post(url, { username, password }));
+    return this.isSuccessfulStatusCode(response.status);
+  };
+
+  public readonly signup = async (payload: ISignUpPayload): Promise<boolean> => {
+    const url = Endpoints.Auth.signup();
+    const response = await this.asyncRunner(
+      () => this.api.post(url, payload))
     return this.isSuccessfulStatusCode(response.status);
   };
 }
