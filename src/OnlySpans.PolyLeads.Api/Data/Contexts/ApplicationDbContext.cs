50c8f0ce9f52ea2cb776f64c047dac5e38efdfc2
aminiakhmetov <aminiakhmetov@data-platform.ru>
2024-05-16 23:55:54 +0300

    added url permission checks

--- src/OnlySpans.PolyLeads.Api/Data/Contexts/ApplicationDbContext.cs
+++ src/OnlySpans.PolyLeads.Api/Data/Contexts/ApplicationDbContext.cs
@@ -10,7 +10,10 @@ public class ApplicationDbContext :
 {
     public DbSet<Document> Documents { get; init; } = default!;
 
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    public DbSet<PermittedSource> PermittedSources { get; init; } = default!;
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
