4b78417e09c59924cbe757a1840d7944ea9120b4
kasyanof.artem <kasyanof.artem2017@yandex.ru>
2024-05-18 13:21:48 +0300

    Added MasterRoleOptions

--- src/OnlySpans.PolyLeads.Api/Startup/Startup.Databases.cs
+++ src/OnlySpans.PolyLeads.Api/Startup/Startup.Databases.cs
@@ -1,5 +1,4 @@
 using Marten;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Npgsql;
@@ -86,67 +85,5 @@ public static partial class Startup
         if (!dbContext.Database.IsRelational()) return;
 
         await dbContext.Database.MigrateAsync();
-    }
-
-    private static async Task<WebApplication> SeedUserRolesAsync(this WebApplication app)
-    {
-        using var scope = app.Services.CreateScope();
-        var provider = scope.ServiceProvider;
-
-        var roleManager = provider.GetRequiredService<RoleManager<Entities.ApplicationUserRole>>();
-
-        var roles = new List<Entities.ApplicationUserRole>
-        {
-            new() { Name = "Admin" },
-            new() { Name = "StudentUnionOrganizer" },
-            new() { Name = "Headman" },
-            new() { Name = "Student" }
-        };
-
-        foreach (var role in roles)
-        {
-            if (!await roleManager.RoleExistsAsync(role.Name!))
-            {
-                var result = await roleManager.CreateAsync(role);
-
-                if (!result.Succeeded)
-                    throw new Exception($"Ошибка при создании роли {role.Name}.");
-            }
-        }
-
-        return app;
-    }
-
-    private static async Task SeedMasterUserAsync(this WebApplication app)
-    {
-        using var scope = app.Services.CreateScope();
-        var provider = scope.ServiceProvider;
-
-        var userManager = provider.GetRequiredService<UserManager<Entities.ApplicationUser>>();
-
-        var masterUser = await userManager.FindByNameAsync("Berkas");
-
-        if (masterUser is not null)
-        {
-            var hasRightPassword = await userManager.CheckPasswordAsync(masterUser, "svo12345");
-
-            var hasAdminRole = await userManager.IsInRoleAsync(masterUser,"Admin");
-
-            if (hasRightPassword && hasAdminRole)
-                return;
-
-            await userManager.DeleteAsync(masterUser);
-        }
-
-        masterUser = new Entities.ApplicationUser
-        {
-            FirstName = "sex",
-            LastName = "void",
-            UserName = "Berkas"
-        };
-
-        await userManager.CreateAsync(masterUser, "svo12345");
-
-        await userManager.AddToRoleAsync(masterUser, "Admin");
     }
 }
