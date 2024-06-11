642f4f642cd0e4c84af383701b63eb59e1966e31
milkyLake <ajnaliev07@gmail.com>
2024-06-11 18:09:04 +0300

    added adaptive

--- src/onlyspans.polyleads.ui/src/components/chat-bot/chat-bot.tsx
+++ src/onlyspans.polyleads.ui/src/components/chat-bot/chat-bot.tsx
@@ -5,7 +5,7 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 
 const ChatBot: FC = () => {
   return (
-    <div className='w-1/2 flex flex-col items-center content-center justify-between'>
+    <div className='md:w-1/2 px-4 w-full flex flex-col items-center content-center justify-between'>
       <ScrollArea className='w-full h-screen rounded-md border p-4 mb-6'>
         so funny that they couldn't help but laugh. And once they started
       </ScrollArea>
