3e0229a1051a7c961b72b6be439a3efa43d9b3ab
kasyanof.artem <kasyanof.artem2017@yandex.ru>
2024-06-21 17:49:45 +0300

    Added LLM settings in json and LLMOptions

---
+++ src/OnlySpans.PolyLeads.Api/Data/Options/LLMOptions.cs
@@ -0,0 +1,18 @@
+﻿using OnlySpans.PolyLeads.Api.Data.Abstractions.Options;
+
+namespace OnlySpans.PolyLeads.Api.Data.Options;
+
+public sealed record LLMOptions() : IApplicationOptions
+{
+    public static string Section { get; } = "LLM";
+
+    public static string ApiKey { get; } = string.Empty;
+
+    public static string ModelUri { get; } = string.Empty;
+
+    public static string RequestUri { get; } = string.Empty;
+
+    public static float Temperature { get; } = 0;
+
+    public static int MaxResponseTokens { get; } = 0;
+}
