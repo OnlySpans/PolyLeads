082c508ceee2a69fd36c384f7a204bf14f00bc28
milkyLake <ajnaliev07@gmail.com>
2024-06-21 16:00:34 +0300

    rewrote autoscroll

--- src/onlyspans.polyleads.ui/src/components/chat-bot/request-form/request-form.tsx
+++ src/onlyspans.polyleads.ui/src/components/chat-bot/request-form/request-form.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { observer } from 'mobx-react-lite';
-import React, { FC, useEffect, useRef } from 'react';
+import React, { FC } from 'react';
 import useGet from '@/hooks/useGet';
 import { IRequestFormVM } from '@/components/chat-bot/request-form/request-form.vm';
 import ServiceSymbols from '@/data/constant/ServiceSymbols';
@@ -13,21 +13,12 @@ import { IChatBotVM } from '@/components/chat-bot/chat-bot.vm';
 const RequestForm: FC = () => {
   const vm = useGet<IRequestFormVM>(ServiceSymbols.IRequestFormVM);
   const chatBotVM = useGet<IChatBotVM>(ServiceSymbols.IChatBotVM);
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-
-  useEffect(() => {
-    const textarea = textareaRef.current;
-    if (textarea) {
-      textarea.style.height = 'auto';
-      textarea.style.height = `${textarea.scrollHeight}px`;
-    }
-  }, [vm.value]);
 
   return (
     <div className='flex gap-4 items-end'>
       <Textarea
         id='requestForm'
-        ref={textareaRef}
+        ref={vm.textareaRef}
         value={vm.value}
         disabled={chatBotVM.isLoading}
         onChange={(e) => vm.setValue(e.target.value)}
--- src/onlyspans.polyleads.ui/src/components/chat-bot/request-form/request-form.vm.ts
+++ src/onlyspans.polyleads.ui/src/components/chat-bot/request-form/request-form.vm.ts
@@ -2,12 +2,15 @@ import { inject, injectable } from 'inversify';
 import { action, makeObservable, observable } from 'mobx';
 import ServiceSymbols from '@/data/constant/ServiceSymbols';
 import type { IChatBotVM } from '@/components/chat-bot/chat-bot.vm';
+import type { RefObject } from 'react';
+import { createRef } from 'react';
 
 export interface IRequestFormVM {
   value: string;
   setValue: (value: string) => void;
   sendRequest: () => void;
   sendExampleRequest: (request: string) => void;
+  textareaRef: RefObject<HTMLTextAreaElement>;
 }
 
 @injectable()
@@ -17,6 +20,10 @@ class RequestFormVM implements IRequestFormVM {
 
   private readonly chatBotVM: IChatBotVM;
 
+  @observable
+  public readonly textareaRef: RefObject<HTMLTextAreaElement> =
+    createRef<HTMLTextAreaElement>();
+
   constructor(@inject(ServiceSymbols.IChatBotVM) chatBotVM: IChatBotVM) {
     this.chatBotVM = chatBotVM;
     makeObservable(this);
@@ -25,6 +32,12 @@ class RequestFormVM implements IRequestFormVM {
   @action
   public setValue = (value: string) => {
     this.value = value;
+
+    const textarea = this.textareaRef.current;
+    if (textarea) {
+      textarea.style.height = 'auto';
+      textarea.style.height = `${textarea.scrollHeight}px`;
+    }
   };
 
   @action
